116d946943e8115861791da47a0ee70b0ca1d34e
YJH <m17854239497@outlook.com>
2018-04-23 21:19:01 +0800

    改变背景

--- vue-junmall/build/webpack.prod.conf.js
+++ vue-junmall/build/webpack.prod.conf.js
@@ -63,7 +63,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: config.build.index,
       template: 'index.html',
-      inject: true,
+      inject: false,
       minify: {
         removeComments: true,
         collapseWhitespace: true,
