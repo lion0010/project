e47f01ec96f3f10ff74e492cd2b5c52f9700933a
lion0010 <18661688379@163.com>
2018-05-13 23:10:40 +0800

    goodsDetailsPage

--- vue-junmall/src/views/GoodsShop.vue
+++ vue-junmall/src/views/GoodsShop.vue
@@ -4,13 +4,18 @@
       <nav-bread>
         <span>商品详情</span>
       </nav-bread>
-      <div v-for="(item,index) in GoodsShop">
+      <!-- <div v-for="(item,index) in GoodsShop" :key="index"> -->
         <el-row  class="Shop_Con">
-            <el-col :md="8" :sm='12'>
-              <div class="Shop_img" >
-                <img v-lazy="item.productImage" width="100%">
+          <el-col :md="8" :sm='12'>
+            <div class="Shop_img" >
+              <div id="smallPic" class="smallPic" :ref="'smallPic'">
+                <img :src="item.productImage" width="300" height="300" alt="">
+                <div id="zoom" class="zoom" :ref="'zoom'">
+                </div>
               </div>
-            </el-col>
+              <div id="bigPic" class="bigPic" :style="{backgroundImage: 'url('+item.productImage+')'}" :ref="'bigPic'"></div>
+            </div>
+          </el-col>
             <el-col :md="10" :sm='12'>
               <div class="Shop_Title">
                 {{item.productName}}
@@ -23,11 +28,8 @@
               <div class="Shop_s">
                 <p>颜色/款式：默认</p>
               </div>
-              <div class="Shop_s">
+                <div class="Shop_s">
                 <p>尺码/规格：默认</p>
-              </div>
-              <div class="Shop_s">
-                <p>销量：{{ item.sales || 0 }}</p>
               </div>
               <div></div>
               <div>
@@ -52,9 +54,9 @@
             </el-col>
               <el-col :md="6" class='hidden-sm-and-down'>
                   <h3 class=" text_center ">猜你喜欢</h3>
-                  <div class="like" v-for="(item,index) in foods"  @click="shopgo(item.productId)">
-                      <img v-lazy="item.productImage" width="100%">
-                      <p> {{item.productName}}</p>
+                  <div class="like" v-for="(itemLike,index) in foods"  @click="shopgo(item.productId)">
+                      <img v-lazy="itemLike.productImage" width="100%">
+                      <p> {{itemLike.productName}}</p>
                 </div>
               </el-col>
         </el-row>
@@ -65,7 +67,7 @@
                 <el-col :md="24" class='Shop_text' >
                   <p>{{item.details}}</p>
                   <img v-lazy="item.productImage" >
-                  <p> <img src="../../static/details.jpg" width="100%"></p>  
+                  <p> <img src="../../static/details.jpg" width="100%"></p>
                 </el-col>
               </el-row>
             </el-tab-pane>
@@ -74,7 +76,7 @@
             </el-tab-pane>
           </el-tabs>
         </div>
-      </div>
+      <!-- </div> -->
 
       <modal v-bind:mdShow="mdShow" v-on:close="closeMoadl">
         <p slot="message">
@@ -98,7 +100,7 @@
       </modal>
       <nav-footer></nav-footer>
       <transition name="show">
-        <to-top></to-top>  	
+        <to-top></to-top>
       </transition>
     </div>
 </template>
@@ -234,6 +236,39 @@
 .show-info .el-tabs__item:hover {
   color: #d1434a;
 }
+/* 放大镜 */
+.smallPic {
+  position: relative;
+  width: 300px;
+  height: 300px;
+  /* margin-left: 100px; */
+  border: 0px solid #ccc;
+}
+.smallPic .zoom {
+  position: absolute;
+  width: 175px;
+  height: 175px;
+  top: 100px;
+  left: 100px;
+  background-color: #fede4f;
+  opacity: 0.5;
+  filter: alpha(opacity=50);
+  border: 1px solid #aaa;
+  display: none;
+}
+.bigPic {
+  width: 300px;
+  height: 300px;
+  position: absolute;
+  background-size: 600px 600px;
+  background-repeat: no-repeat;
+  /* background-image: url({{item.productImage}}); */
+  top: 20px;
+  left: 350px;
+  border: 1px solid black;
+  z-index: 9999;
+  display: none;
+}
 </style>
 <script>
 import "../assets/css/product.css";
@@ -243,8 +278,8 @@ import NavHeader from "@/components/Header";
 import NavFooter from "@/components/Footer";
 import NavBread from "@/components/Bread";
 import Modal from "@/components/Modal";
-import toTop from '@/components/toTop'
-import UserComment from "@/components/userComment"
+import toTop from "@/components/toTop";
+import UserComment from "@/components/userComment";
 import "element-ui/lib/theme-chalk/display.css";
 import axios from "axios";
 export default {
@@ -261,7 +296,7 @@ export default {
       filterby: false,
       num: 1,
       page: 1,
-      activeName: 'productDetail'
+      activeName: "productDetail"
     };
   },
   //使用组件
@@ -273,8 +308,14 @@ export default {
     toTop,
     UserComment
   },
-  mounted() {
+  created() {
+    // this.$router.go(0);
     this.init(); //初始化
+  },
+  computed: {
+    item() {
+      return this.GoodsShop.length === 1 ? this.GoodsShop[0] : ''
+    }
   },
   methods: {
     init() {
@@ -297,11 +338,11 @@ export default {
           this.food();
           this.page = Math.ceil(Math.random() * 10);
         }
+        this.zoom()
       });
     },
     //加入购物车
     addCart(productId) {
-      console.log(productId);
       axios
         .post("/goods/addCart", { productId: productId, num: this.num })
         .then(res => {
@@ -334,7 +375,6 @@ export default {
       this.init();
     },
     pay(productId) {
-      console.log(this.$store.state.mdShow);
       if (this.$store.state.mdShow) {
         this.mdShow = true;
       } else {
@@ -352,6 +392,98 @@ export default {
     },
     closeCart() {
       this.mdShowCart = false;
+    },
+
+    // 绑定放大镜效果
+    zoom() {
+        var oSmallPic = document.querySelector("#smallPic");
+        var oBigPic = document.querySelector("#bigPic");
+        var oZoom = document.querySelector("#zoom");
+        console.log(oSmallPic)
+        //大图800*800 大图盒子 400*400
+        //小图盒子350*350 放大镜175*175
+        //所以放大镜总行程是350-175 = 175,  大图的总行程 800 - 400 = 400
+        // var rate = 400 / 175;//可以用这句话代替下面的四行，下面四行是更通用的代码
+        var bigPicWidth = parseFloat(fetchComputedStyle(oBigPic, "width"));
+        var smallPicWidth = parseFloat(fetchComputedStyle(oSmallPic, "width"));
+        var zoomWidth = parseFloat(fetchComputedStyle(oZoom, "width"));
+        var rate = (600 - bigPicWidth) / (smallPicWidth - zoomWidth);
+
+        oSmallPic.onmouseover = function() {
+          oZoom.style.display = "block";
+          oBigPic.style.display = "block";
+        };
+        oSmallPic.onmouseout = function() {
+          oZoom.style.display = "none";
+          oBigPic.style.display = "none";
+        };
+
+        oSmallPic.onmousemove = function(event) {
+          event = event || window.event;
+
+          //event.offsetX不能用
+          //因为onmousemove事件冒泡，鼠标碰到zoom这个放大镜时事件将往上传播
+          //会触发oSmallPic的onmousemove事件。因此event.offsetX的坐标，以zoom左上角为准
+          // var x = event.offsetX;
+          // var y = event.offsetY;
+
+          var scrollLeft =
+            document.body.scrollLeft || document.documentElement.scrollLeft;
+          var scrollTop =
+            document.body.scrollTop || document.documentElement.scrollTop;
+
+          var x =
+            event.clientX -
+            (getAllLeft(oSmallPic) - scrollLeft) -
+            oZoom.clientWidth / 2;
+          var y =
+            event.clientY -
+            (getAllTop(oSmallPic) - scrollTop) -
+            oZoom.clientHeight / 2;
+          if (x < 0) x = 0;
+          if (y < 0) y = 0;
+          if (x > oSmallPic.clientWidth - oZoom.clientWidth) {
+            x = oSmallPic.clientWidth - oZoom.clientWidth;
+          }
+          if (y > oSmallPic.clientHeight - oZoom.clientHeight) {
+            y = oSmallPic.clientHeight - oZoom.clientHeight;
+          }
+
+          oZoom.style.top = y + "px";
+          oZoom.style.left = x + "px";
+
+          oBigPic.style.backgroundPosition = -x * rate + "px " + -y * rate + "px";
+        };
+        function fetchComputedStyle(obj, property) {
+          if (window.getComputedStyle) {
+            property = property.replace(/[A-Z]/g, function(match) {
+              return "-" + match.toLowerCase();
+            });
+            return window.getComputedStyle(obj)[property]; //中括号里面可以是变量
+          } else {
+            property = property.replace(/-([a-z])/g, function(match, $1) {
+              return $1.toUpperCase();
+            });
+            return obj.currentStyle[property];
+          }
+        }
+        function getAllTop(obj) {
+          var allTop = obj.offsetTop;
+          var currentObj = obj;
+          while ((currentObj = currentObj.offsetParent)) {
+            allTop += currentObj.offsetTop;
+          }
+          return allTop;
+        }
+        function getAllLeft(obj) {
+          var allLeft = obj.offsetLeft;
+          var currentObj = obj;
+          while ((currentObj = currentObj.offsetParent)) {
+            allLeft += currentObj.offsetLeft;
+          }
+          return allLeft;
+        }
+
     }
   }
 };
