ce8fbd025c73838e9efba183f368b4d4ec1fd64e
YJH <m17854239497@outlook.com>
2018-05-17 15:39:03 +0800

    删除dist

--- vue-junmall/config/api.config.js
+++ vue-junmall/config/api.config.js
@@ -4,5 +4,5 @@ const isProdMode = Object.is(process.env.NODE_ENV, 'development')
 // console.log(process.env.NODE_ENV)
 // console.log(isProdMode)
 module.exports = {
-  baseUrl: isProdMode ? 'api/' : 'http://yjh.vnshopping.top/api/' 
+  baseUrl: isProdMode ? 'api/' : 'http://wanlu.lao7.site/api/' 
 }
